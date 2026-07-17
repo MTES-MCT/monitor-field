7458b9ef3e03396e08d0464ef9ec2f18668d4653
Claire Dagan <claire.dagan.pro@gmail.com>
2026-07-17 14:46:02 +0200

    [Test] update tests

--- src/utils/__tests__/parseWtkToGeojson.test.ts
+++ src/utils/__tests__/parseWtkToGeojson.test.ts
@@ -3,18 +3,19 @@
 import { parseWktToGeojson } from '@utils/parseWktToGeojson'
 
 describe('parseWktToGeojson', () => {
-  it('parses a polygon into a GeoJSON Feature', () => {
-    const input = 'SRID=4326POLYGON((0 0, 10 0, 10 10, 0 0))'
+  it('parses a multipolygon into a GeoJSON Feature', () => {
+    const input =
+      'MULTIPOLYGON(((-3.43414 48.83543,-3.41475 48.83543,-3.41475 48.83016,-3.43414 48.83016,-3.43414 48.83543)))'
 
     const feature = parseWktToGeojson(input)
 
     expect(feature).toBeDefined()
     expect(feature?.type).toBe('Feature')
-    expect(feature?.geometry.type).toBe('Polygon')
+    expect(feature?.geometry.type).toBe('MultiPolygon')
   })
 
   it('returns undefined on invalid WKT', () => {
-    const input = 'SRID=4326POLYGON((0 0, 10))'
+    const input = 'POLYGON((0 0, 10))'
 
     expect(parseWktToGeojson(input)).toBeUndefined()
   })
