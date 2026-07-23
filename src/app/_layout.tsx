68e8c551798fe07e3593dd4ee8f9a74c4f3609df
Claire Dagan <claire.dagan.pro@gmail.com>
2026-07-23 14:06:30 +0200

    [Location] listen if user enabled or disabled gps on his phone when app is open

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 import { AppProvider } from '@contexts/AppContext'
@@ -8,8 +8,6 @@ import { RegulatoryAreasProvider } from '@contexts/RegulatoryAreasContext'
 import { syncFishRegulatoryAreasDB } from '@features/RegulatoryAreas/useCases/syncFishRegulatoryAreasDB'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 import { useAppColorScheme } from '@hooks/use-app-color-scheme'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import * as Location from 'expo-location'
 import { Appearance } from 'react-native'
 import App from '.'
 
@@ -26,30 +24,9 @@ const getFishLayers = async () => {
 
 export default function TabLayout() {
   const colorScheme = useAppColorScheme()
-  const [isLocationEnabledState, setIsLocationEnabledState] = useState(false)
+
   useEffect(() => {
     Appearance.setColorScheme('light')
-  }, [])
-
-  useEffect(() => {
-    async function getCurrentLocation() {
-      // request permissions for use location
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted') {
-        await AsyncStorage.setItem('is-location-granted', 'false')
-        await AsyncStorage.setItem('is-location-activated', 'false')
-        return
-      }
-      await AsyncStorage.setItem('is-location-granted', 'true')
-
-      // check if location is activated on the device
-      const isEnabled = await Location.hasServicesEnabledAsync()
-
-      setIsLocationEnabledState(isEnabled)
-      await AsyncStorage.setItem('is-location-activated', isEnabled ? 'true' : 'false')
-    }
-
-    getCurrentLocation()
   }, [])
 
   useEffect(() => {
@@ -64,7 +41,7 @@ export default function TabLayout() {
         <RegulatoryAreasProvider>
           <BottomSheetModalProvider>
             <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-              <App isFirstLocationEnabled={isLocationEnabledState} />
+              <App />
             </ThemeProvider>
           </BottomSheetModalProvider>
         </RegulatoryAreasProvider>
